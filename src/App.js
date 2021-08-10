b3f715ab02960dd1f5c242079306cd6fe24e34d5
engineer135 <everafterk@naver.com>
2021-08-10 20:17:35 +0900

    url 파라미터 사용해보기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   // Route 태그에 exact true로 설정해줘야 about 들어갔을때 home 안나온다. 설정 안하면 '/about'과 '/' 이 부분이 겹쳐서 home이 중복으로 나옴.
@@ -16,9 +17,16 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true}/>
       <Route path="/about" component={About}/>
+      <Route path="/profile/:username" component={Profile}/>
     </div>
   );
 }
